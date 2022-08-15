fca34f3ea244c38980ce52d654a5e7a2fdabc461
Leonardo Kratz Mendes <kratzmendes@gmail.com>
2022-08-15 16:16:39 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_vKuBR602yWmAFf1u5VOJ0ZdGkmUqqK8",
+  authDomain: "fir-recipes-2ee87.firebaseapp.com",
+  projectId: "fir-recipes-2ee87",
+  storageBucket: "fir-recipes-2ee87.appspot.com",
+  messagingSenderId: "93183925076",
+  appId: "1:93183925076:web:b3eec14dd1e2c2192946c1",
+  measurementId: "G-VZDRJQ587F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
